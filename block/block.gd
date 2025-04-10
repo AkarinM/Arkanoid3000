4687982b125d2cf9b5851fd1fb37c304abf8f734
Akarin <mihailmoro96@yandex.ru>
2025-04-10 22:05:34 +0300

    настроил сцену для обработки столкновений. Добавил функцию для уничтожения блока при попадании мячом

--- block/block.gd
+++ block/block.gd
@@ -8,3 +8,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == 'Ball':
+		queue_free()
+	pass # Replace with function body.
