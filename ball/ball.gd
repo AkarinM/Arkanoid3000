6eb1974a150e971a787084816c60dde0f2f287a1
Akarin <mihailmoro96@yandex.ru>
2025-04-10 21:41:57 +0300

    ы

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,15 +2,13 @@ extends RigidBody2D
 
 @export var speed: int = 100
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	linear_velocity = Vector2.DOWN * speed
-	
-	pass
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+		
