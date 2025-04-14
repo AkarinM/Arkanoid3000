f382a72f0f1c95ce39c03f75506bf90484fdb171
Akarin <mihailmoro96@yandex.ru>
2025-04-14 22:45:37 +0300

    сцена с победой, логика показа сцены победы

--- level/level.gd
+++ level/level.gd
@@ -1,34 +1,46 @@
 extends Node2D
 
 var win = false
+var loose = false
+
+var num_blocks = 0
+
+@export var win_text = 'You win!'
+@export var loose_text = 'You loose!'
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Signals_bus.block_destroyed.connect(_on_block_destroyed)
+	
+	#for child in get_children():
+		#if child.has_signal("ready"):
+			#await child.ready
+			
+	num_blocks = get_tree().get_nodes_in_group('blocks').size()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if win:
-		you_win()
+	if win or loose:
+		end_game()
 
-
-func count_blocks() -> int:
-	return get_tree().get_nodes_in_group('blocks').size()
-	
 
 func _on_block_destroyed():
-	var count = count_blocks()
-	print('count', count)
+	num_blocks -= 1
 	
-	if count == 0:
-		print('dddddddd')
+	print('num_blocks', num_blocks)
+	
+	if num_blocks <= 0:
 		win = true
-		
 
-func you_win():
+
+func end_game():
 	$Ball.hide()
 	$Board.hide()
 	
-	print('YOU WIN!!!')
+	var label = preload('res://label/label.tscn').instantiate()
+	var text = win_text if win else loose_text
 	
+	label.text = text
+	
+	add_child(label)
