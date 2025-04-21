bd7a24ccd952402be2ba1e64bbe405e0d8ffcfeb
Akarin <mihailmoro96@yandex.ru>
2025-04-21 22:40:13 +0300

    подсчет и вывод затраченного на уровень времени

--- level/level.gd
+++ level/level.gd
@@ -5,6 +5,7 @@ var loose = false
 
 var num_blocks = 0
 var board_pos_y = 0
+var wait_time = 0
 
 @export var win_text = 'You win!'
 @export var loose_text = 'You loose!'
@@ -17,6 +18,7 @@ func _ready() -> void:
 	num_blocks = get_tree().get_nodes_in_group('blocks').size()
 	board_pos_y = $Board.position.y
 	
+	wait_time = $Timer.wait_time
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,3 +43,9 @@ func _on_ball_dawn(pos_y):
 func end_game():
 	Common.change_scene('res://loose/loose.tscn')
 	
+
+
+func _on_timer_timeout() -> void:
+	var val = int($cur_time.text)
+	
+	$cur_time.text = str(val + wait_time)
