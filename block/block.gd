c11770ce32a5aa04f9ac90d97fc6770a19be3251
Akarin <mihailmoro96@yandex.ru>
2025-07-22 23:24:10 +0300

    подсчет очков за разрушение блоков

--- block/block.gd
+++ block/block.gd
@@ -1,7 +1,14 @@
 extends RigidBody2D
 
+var val_score = 0
+
+func _init():
+	var rnd = RandomNumberGenerator.new()
+	
+	val_score = rnd.randi_range(10, 30)
+
 func _on_body_entered(body: Node) -> void:
 	if body.name == 'Ball':
 		queue_free()
 		await tree_exited
-		Signals_bus.block_destroyed.emit()
+		Signals_bus.block_destroyed.emit(val_score)
--- win/label.gd
+++ win/label.gd
@@ -1,7 +1,11 @@
 extends Label
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	text += ' ' + str(Common.time)
+	var text_score = 'Score: {0}'.format([Common.score])
+	var text_time = 'Time: {0}'.format([Common.time])
+	text = """
+	{0}
+	{1}
+	{2}
+	""".format([text, text_score, text_time])
